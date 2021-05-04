801ca7dedacc3f573eca4cefd19ef396b7c97e3b
Antonio Berthfield <antonio@berthfield.com>
2021-05-03 23:03:10 -0500

    Deployed:  Twilio fully working to gmail  Beginning test of anveo

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import os
 import datetime
 import json
 
+# import flask logging
+import logging
+from logging import config
+from flask_google_cloud_logger import FlaskGoogleCloudLogger
+
+
 # Imports for the twilio sms endpoint
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-# from validate_email import validate_email
+
+LOG_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "json": {
+            "()": "flask_google_cloud_logger.FlaskGoogleCloudFormatter",
+            "application_info": {
+                "type": "python-application",
+                "application_name": "Example Application"
+            },
+            "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+        }
+    },
+    "handlers": {
+        "json": {
+            "class": "logging.StreamHandler",
+            "formatter": "json"
+        }
+    },
+    "loggers": {
+        "root": {
+            "level": "INFO",
+            "handlers": ["json"]
+        },
+        "werkzeug": {
+            "level": "WARN",  # Disable werkzeug hardcoded logger
+            "handlers": ["json"]
+        }
+    }
+}
 
 # Create sendgrid instance
 sg = SendGridAPIClient(api_key=os.environ["SENDGRID_API_KEY"])
 
 # Create flask instance
 app = Flask(__name__)
+
+config.dictConfig(LOG_CONFIG)  # load log config from dict
+logger = logging.getLogger("root")  # get root logger instance
+FlaskGoogleCloudLogger(app)
+
 
 # Set attribute to mitigate error in dev environ... Not sure of the root cause
 app.__name__ = "__main__"
@@ -27,8 +67,10 @@ def email_lookup(num):
     with open("num_sheet.json") as data:
         num_dict = json.load(data)
         to_email = {value for (key, value) in num_dict.items() if str(num) == key}
-        return to_email.pop()
-
+        try:
+            return to_email.pop()
+        except KeyError:
+            return False
 
 def send_email(to_email, txt_from, txt_to, txt_body):
     """Send a templated email with SendGrid."""
@@ -36,19 +78,16 @@ def send_email(to_email, txt_from, txt_to, txt_body):
         from_email = f"{txt_from}@berthfield.com",
         to_emails = f"{to_email}",
         subject = f'SMS from {txt_from} for {txt_to}.',
-        html_content = f'{txt_to}'
+        html_content = f'{txt_body}'
     )
 
     try:
         response = sg.send(mail)
         # print(response)
         print(response.status_code, response.body, response.headers)
+        return response.status_code
     except Exception as e:
         print(e.message)
-    #sg.client.mail.send.post(request_body=mail.get())
-    #print(f"from email: {mail.from_email}")
-    #print(f"To: {mail.to_emails}")
-    #print(f"body: {mail.html_content}")
 
 
 # HomePage
@@ -57,42 +96,61 @@ def homepage():
     return render_template("index.html", present_date=formatted_date)
 
 
-@app.route("/sms", methods=['GET', 'POST'])
-def incoming_sms():
+@app.route("/sms-twil", methods=['GET', 'POST'])
+def incoming_twil_sms():
     """Respond to SMS."""
-    plat = request.args['provider']
+    sms_from = request.form['From']
+    print(f"from: {sms_from} type: {type(sms_from)}")
+    sms_to = request.form['To']
+    sms_txt = request.form['Body']
+    address = email_lookup(sms_to)
 
-    print(plat)
-
-    if plat == 'twilio':
-        sms_from = request.args['From']
-        sms_to = request.args['To']
-        sms_txt = request.args['Body']
-
-        address = email_lookup(sms_to)
-        if address:
-            send_email(to_email=address, txt_from=sms_from, txt_body=sms_txt, txt_to=sms_to)
-
-    elif plat == 'anveo':
-        sms_from = request.args['from']
-        sms_to = request.args['to']
-        sms_txt = request.args['message']
-
-        address = email_lookup(sms_to)
-        if address:
-            send_email(to_email=address, txt_from=sms_from, txt_body=sms_txt, txt_to=sms_to)
-
+    if address:
+        return str(send_email(to_email=address, txt_from=sms_from, txt_body=sms_txt, txt_to=sms_to))
     else:
-        return (
-            "<Response><Message>"
-            f"Invalid Provider.{plat}"
-            "</Message></Response>"
-        )
+        return "invalid number"
 
 
-# email = email_lookup(sms_to)
-    #
+@app.route("/sms-anv", methods=['GET', 'POST'])
+def incoming_anv_sms():
+    request_args = {key:value for (key,value) in request.args.items()}
+    logging.warning(request_args)
+    print(request_args)
 
-# # Run Server
+    """Respond to SMS."""
+    print(f'Provider: Anveo')
+    sms_from = request.args['from']
+    print(f"from: {sms_from} type: {type(sms_from)}")
+    sms_to = request.args['to']
+    print(f"to: {sms_to} type: {type(sms_to)}")
+    sms_txt = request.args['message']
+    print(f"to: {sms_to} type: {type(sms_to)}")
+    address = email_lookup(sms_to)
+
+    if address:
+        return str(send_email(to_email=address, txt_from=sms_from, txt_body=sms_txt, txt_to=sms_to))
+    else:
+        return "invalid number"
+
+
+@app.teardown_request # log request and response info after extension's callbacks
+def log_request_time(_exception):
+    logger.info(
+        f"{request.method} {request.path} - Sent {g.response.status_code}" +
+        " in {g.request_time:.5f}ms")
+
+
+# Enable Debug
+app.debug = True
+
+
+# Run Server
 # if app.__name__ == "__main__":
 #     app.run(debug=True)
+
+
+
+
+
+
+
